1448f725ca22ba1249ad51ba5619d01770fece21
Dmitry-Kuzmin456 <dmitkuz0@gmail.com>
2025-09-25 21:24:45 +0300

    before deploy

--- src/main/java/server/ResultsBean.java
+++ src/main/java/server/ResultsBean.java
@@ -1,7 +1,7 @@
 package server;
 
-import jakarta.enterprise.context.SessionScoped;
-import jakarta.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/server/servlets/ControllerServlet.java
+++ src/main/java/server/servlets/ControllerServlet.java
@@ -1,9 +1,9 @@
 package server.servlets;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import server.Result;
 
 @WebServlet("/request")
